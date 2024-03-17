60b5e5fc206baca10ad548a2101b8b00a3775324
EduardoGhioldi <ghioldigahonaeduardo@gmail.com>
2024-03-17 20:27:35 -0300

    Bootstrap installed

--- AddresBookWeb/src/app/contact-list/contact-list.component.ts
+++ AddresBookWeb/src/app/contact-list/contact-list.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ContactService } from '../services/contact.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
 export default class ContactListComponent implements OnInit{
   private contactService = inject(ContactService);
 
+  contacts: any[] = [];
+
   ngOnInit(): void {
     this.contactService.list()
-      .subscribe(contacts => {
-        console.log(contacts);
-      } )
+      .subscribe((contacts: any) => {
+          this.contacts = contacts;
+            } )
   }
 }
